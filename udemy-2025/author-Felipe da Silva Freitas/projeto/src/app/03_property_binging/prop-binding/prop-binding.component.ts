d0e84d23c301da9612906ff00238121cb0e5c1eb
andecodelima <andrelimag89@gmail.com>
2025-03-24 14:08:36 -0300

    feat:propert and event Binding

---
+++ udemy-2025/author-Felipe da Silva Freitas/projeto/src/app/03_property_binging/prop-binding/prop-binding.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prop-binding',
+  imports: [],
+  templateUrl: './prop-binding.component.html',
+  styleUrl: './prop-binding.component.scss'
+})
+export class PropBindingComponent {
+
+  inputText = "Texto Inicial Alterado";
+  inputType = "text";
+  isDisabled = true;
+  
+  enableInput(){
+    this.isDisabled = false;
+  }
+
+  disabledInput(){
+    this.isDisabled = true;
+  }
+
+  setText(){
+    this.inputType = "text";
+  }
+
+  setPassword(){
+    this.inputType = "password";
+  }
+}
+
--- udemy-2025/author-Felipe da Silva Freitas/projeto/src/app/app.component.ts
+++ udemy-2025/author-Felipe da Silva Freitas/projeto/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardsComponent } from './aulas/card/card.component';
-import { CardRoxoComponent } from "./aulas/card-roxo/card-roxo.component";
+// import { CardsComponent } from './aulas/card/card.component';
+// import { CardRoxoComponent } from "./aulas/card-roxo/card-roxo.component";
+import { PropBindingComponent } from "./03_property_binging/prop-binding/prop-binding.component";
+import { EventBindingComponent } from "./03_property_binging/event-binding/event-binding.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardsComponent, CardRoxoComponent],
+  imports: [RouterOutlet, EventBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
