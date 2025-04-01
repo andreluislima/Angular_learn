6a87bfeeb7b8be0a940612c96db79ca876b721b1
andecodelima <andrelimag89@gmail.com>
2025-04-01 10:30:26 -0300

    feat:component-userdetails

--- udemy-2025/author-Felipe da Silva Freitas/projetos/lista_usuarios/projeto/src/app/app.component.ts
+++ udemy-2025/author-Felipe da Silva Freitas/projetos/lista_usuarios/projeto/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TesteComponent } from "./components/teste/teste.component";
+import { UserDetailsComponent } from "./components/user-details/user-details.component";
   
 @Component({
   selector: 'app-root',
-  imports: [TesteComponent],
+  imports: [TesteComponent, UserDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
