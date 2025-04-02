394d54ff2e6880328e087009a2c99aa05b6022c2
andecodelima <andrelimag89@gmail.com>
2025-04-02 11:24:59 -0300

    feat: click usuario

--- udemy-2025/author-Felipe da Silva Freitas/projetos/lista_usuarios/projeto/src/app/app.component.ts
+++ udemy-2025/author-Felipe da Silva Freitas/projetos/lista_usuarios/projeto/src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { UserDetailsComponent } from "./components/user-details/user-details.component";
 import { FilterComponent } from "./components/filter/filter.component";
 import { UsersListComponent } from "./components/users-list/users-list.component";
 import { IUser } from './interfaces/user/user.interface';
-import { UsersList } from './data/users-list';
-  
+   
 @Component({
   selector: 'app-root',
-  imports: [UserDetailsComponent, FilterComponent, UsersListComponent],
+  imports: [UserDetailsComponent, FilterComponent, UsersListComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
    userSelected:IUser = {} as IUser; // Preve que o objeto pode vir vazio.
+   showUserDetails:boolean = false;
 
    onUserSelected(user: IUser){
     this.userSelected = user;
+    this.showUserDetails = true;
    }
 }
