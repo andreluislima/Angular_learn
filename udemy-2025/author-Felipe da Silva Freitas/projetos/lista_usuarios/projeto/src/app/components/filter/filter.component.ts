3937d8ec92ea303a2d1af45696af54c0ebb37913
andecodelima <andrelimag89@gmail.com>
2025-04-03 15:35:49 -0300

    feat: conversao data formato BR

--- udemy-2025/author-Felipe da Silva Freitas/projetos/lista_usuarios/projeto/src/app/components/filter/filter.component.ts
+++ udemy-2025/author-Felipe da Silva Freitas/projetos/lista_usuarios/projeto/src/app/components/filter/filter.component.ts
@@ -2,7 +2,6 @@ import {Component,ChangeDetectionStrategy, Output, EventEmitter} from '@angular/
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
 import { IFilterOpions } from '../../interfaces/user/filter-options.interface';
@@ -10,20 +9,20 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
-
-  imports: [MatFormFieldModule,
-            MatInputModule,
-            MatDatepickerModule,
-            MatSelectModule,
-            MatButtonModule,
-            FormsModule
-          ],
-
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+    FormsModule
+  ],
   templateUrl: './filter.component.html',
-  providers: [provideNativeDateAdapter()],
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './filter.component.scss'
 })
+
 export class FilterComponent {
 
   filterOpions:IFilterOpions = {
--- udemy-2025/author-Felipe da Silva Freitas/projetos/lista_usuarios/projeto/src/main.ts
+++ udemy-2025/author-Felipe da Silva Freitas/projetos/lista_usuarios/projeto/src/main.ts
@@ -1,6 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, DateAdapter } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ]
+});
