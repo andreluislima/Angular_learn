ae2852bf38c8de007f4de4e66ab966082a10fb7f
andecodelima <andrelimag89@gmail.com>
2025-04-28 11:04:10 -0300

    feat: aula 292

---
+++ udemy-2025/author-Felipe da Silva Freitas/_15_FormBuilder/projeto/src/app/components/form-array/form-array.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormArray, ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-array',
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './form-array.component.html',
+  styleUrl: './form-array.component.scss'
+})
+export class FormArrayComponent {
+
+  musicasForm = new FormGroup({
+    musicas: new FormArray([
+      new FormControl('', [Validators.required])
+    ]),
+  });
+
+  constructor(){
+    console.log(this.musicasForm);
+  }
+
+  get musicas():FormArray{
+    return this.musicasForm.get('musicas') as FormArray;
+  }
+
+  adicionarMusica() {
+    this.musicas.push( new FormControl('Novo', [Validators.required]))
+  }
+
+  removerMusica(musicaIndex:number) {
+    this.musicas.removeAt(musicaIndex);
+  }
+
+ 
+
+}
