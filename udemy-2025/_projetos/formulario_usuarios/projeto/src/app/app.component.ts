d6e901e871e0404e955d3202f2be3abda72fe807
andreluislima <andrelimag89@gmail.com>
2025-05-07 16:54:05 -0300

    feat:UsersListResponse

--- udemy-2025/_projetos/formulario_usuarios/projeto/src/app/app.component.ts
+++ udemy-2025/_projetos/formulario_usuarios/projeto/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './services/countries.services';
 import { StatesService } from './services/states.services';
 import { CitiesService } from './services/cities.services';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -14,23 +15,28 @@ export class AppComponent implements OnInit {
    constructor(
     private readonly _countriesService:CountriesService,
     private readonly _statesService:StatesService,
-    private readonly _citiesService:CitiesService
+    private readonly _citiesService:CitiesService,
+    private readonly _usersService:UsersService
 
    ){}
    
 
    ngOnInit(){
       this._countriesService.getCountries().subscribe((countriesResponse)=>{
-        console.log('countriesResponse', countriesResponse)
+        console.log('countriesResponse', countriesResponse);
        });
 
        this._statesService.getStates('Brazil').subscribe((statesResponse)=>{
-           console.log('statesResponse', statesResponse)
+           console.log('statesResponse', statesResponse);
        });
 
        this._citiesService.getCities('Brazil', 'Rio de Janeiro').subscribe((citiesResponse)=>{
-        console.log('citiesResponse', citiesResponse)
-       })
+        console.log('citiesResponse', citiesResponse);
+       });
+
+       this._usersService.getUsers().subscribe((usersListResponse)=>{
+        console.log('uusersListResponsese',usersListResponse);
+       });
    }
 
    
